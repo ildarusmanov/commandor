a36c62fed8198b527e2b16d6f1bba6a62c34bc38
Ildar Usmanov <wajox@mail.ru>
2018-02-10 16:19:09 +0300

    update mocks

--- tests/mocks/command_mock.go
+++ tests/mocks/command_mock.go
@@ -1,35 +1,36 @@
 package mocks
 
 import (
-    "github.com/stretchr/testify/mock"
+	"github.com/ildarusmanov/commandor/interfaces"
+	"github.com/stretchr/testify/mock"
 )
 
 type CommandMock struct {
-    mock.Mock
+	mock.Mock
 }
 
 func CreateCommandMock() *CommandMock {
-    return new(CommandMock)
+	return new(CommandMock)
 }
 
 func (m *CommandMock) GetName() string {
-    args := m.Called()
+	args := m.Called()
 
-    return args.Get(0).(string)
+	return args.Get(0).(string)
 }
 
 func (m *CommandMock) GetParamsList() map[string]string {
-    args := m.Called()
+	args := m.Called()
 
-    return args.Get(0).(map[string]string)
+	return args.Get(0).(map[string]string)
+}
+
+func (m *CommandMock) Execute(argsMap map[string]string) interfaces.CommandResult {
+	args := m.Called()
+	return args.Get(0).(interfaces.CommandResult)
 }
 
 func (m *CommandMock) Validate(argsMap map[string]string) bool {
-    args := m.Called()
-    return args.Get(0).(bool)
-}
-
-func (m *CommandMock) Validate(argsMap map[string]string) bool {
-    args := m.Called()
-    return args.Get(0).(bool)
+	args := m.Called()
+	return args.Get(0).(bool)
 }
