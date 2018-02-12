ba60409c105e6079efd8f5fcfbf5d8b0602964c8
Ildar Usmanov <wajox@mail.ru>
2018-02-13 00:30:51 +0300

    fix cs with go fmt

--- services/request_processor_test.go
+++ services/request_processor_test.go
@@ -12,14 +12,14 @@ func TestCreateRequestProcessor(t *testing.T) {
 }
 
 func TestPequestProcessorProcess(t *testing.T) {
-    p := CreateRequestProcessor(validator, executor)
+	p := CreateRequestProcessor(validator, executor)
 
-    resp1, err1 := p.Process(validRequest)
-    resp2, err2 := p.Process(invalidRequest)
+	resp1, err1 := p.Process(validRequest)
+	resp2, err2 := p.Process(invalidRequest)
 
-    assert := assert.New(t)
-    assert.NotNil(resp1)
-    assert.Nil(err1)
-    assert.NotNil(resp2)
-    assert.NotNil(err2)
+	assert := assert.New(t)
+	assert.NotNil(resp1)
+	assert.Nil(err1)
+	assert.NotNil(resp2)
+	assert.NotNil(err2)
 }
