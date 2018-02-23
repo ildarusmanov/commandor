605e80f7e8c71503e4b3ec8313f1710158471c10
Ildar Usmanov <wajox@mail.ru>
2018-02-23 21:38:30 +0300

    add comment

--- services/request_processor_test.go
+++ services/request_processor_test.go
@@ -18,9 +18,13 @@ func TestPequestProcessorProcess(t *testing.T) {
 
 	resp1, err1 := p.Process(validRequest)
 	resp2, err2 := p.Process(invalidRequest)
-
+        // valid request should return valid response
+	// with empty error
 	assert := assert.New(t)
 	assert.NotNil(resp1)
+
+	// invalid request should return nil response
+	// with error
 	assert.Nil(err1)
 	assert.NotNil(resp2)
 	assert.NotNil(err2)
