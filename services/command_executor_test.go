15dea894c38912f16cc5a3fdc0f08ab7b61c5a3d
Ildar Usmanov <wajox@mail.ru>
2018-02-26 23:29:41 +0300

    add comments

--- services/command_executor_test.go
+++ services/command_executor_test.go
@@ -21,8 +21,12 @@ func TestCommandExecutorExecute(t *testing.T) {
 	resAsync, errAsync := e.Execucte(true, cmd, params)
 
 	assert := assert.New(t)
+
+	// sync command should finish without error
 	assert.NotNil(resSync)
 	assert.Nil(errSync)
+
+	// async command should finish without error
 	assert.NotNil(resAsync)
 	assert.Nil(errAsync)
 }
