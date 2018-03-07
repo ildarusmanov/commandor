958a2eb6d84d6c67100c8d70fdf62b1d8d8cfc38
Ildar Usmanov <wajox@mail.ru>
2018-03-07 10:57:55 +0300

    start memory storage implementation

---
+++ storage/memory_storage.go
@@ -0,0 +1,14 @@
+package storage
+
+import (
+    "github.com/ildarusmanov/commandor/interfaces"
+)
+
+type MemoryStorage {
+    options map[string]string
+    commands map[string]interfaces.Command
+}
+
+func CreateMemoryStorage(options map[string]string) *MemoryStorage {
+    return &MemoryStorage{options, make(map[string]interfaces.Command)}
+}
