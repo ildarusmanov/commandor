651d056ffe53e3e1710409cce91fdfda4d735768
Ildar Usmanov <wajox@mail.ru>
2018-03-09 11:04:13 +0300

    implement save command method

--- storage/memory_storage.go
+++ storage/memory_storage.go
@@ -2,13 +2,32 @@ package storage
 
 import (
 	"github.com/ildarusmanov/commandor/interfaces"
+    "sync"
+    "errors"
 )
 
+const commandNotFound = errors.New("Command not found")
+
 type MemoryStorage struct {
+    sync.Mutex
 	options  map[string]string
 	commands map[string]interfaces.Command
 }
 
 func CreateMemoryStorage(options map[string]string) *MemoryStorage {
 	return &MemoryStorage{options, make(map[string]interfaces.Command)}
+}
+
+func (s *MemoryStorage) SaveCommand(c interfaces.Command) (interfaces.Command, error) {
+    s.Lock()
+    s.commands[c.GetName()] = c
+    s.Unlock()
+
+    r, ok := s.commands[c.GetName()]
+
+    if !ok {
+        return nil, commandNotFound
+    }
+
+    return r, nil
 }
