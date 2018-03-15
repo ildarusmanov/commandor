510862ba9ed09d7aa4f0d28901d52e14d85fc08f
Ildar Usmanov <wajox@mail.ru>
2018-03-15 11:10:11 +0300

    start command manager implementation

---
+++ services/command_manager.go
@@ -0,0 +1,13 @@
+package services
+
+import (
+    "github.com/ildarusmanov/commandor/interfaces"
+)
+
+type CommandManager struct {
+    storage interfaces.Storage
+}
+
+func CreateCommandManager(storage interfaces.Storage) *CommandManager {
+    return &CommandManager{storage}
+}
